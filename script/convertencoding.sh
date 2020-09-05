ed195d42ecf862b7e432fac2aa5ba05191e8e8ad
John D'Orazio <priest@johnromanodorazio.com>
2020-09-06 00:42:39 +0200

    update encode script

--- script/convertencoding.sh
+++ script/convertencoding.sh
@@ -5,12 +5,16 @@ FROM_ENCODING="ISO-8859-1"
 TO_ENCODING="UTF-8//TRANSLIT"
 #convert
 CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
+mkdir ../src/iso-encoded
+
 #loop to convert multiple files 
 for  file  in  ../src/*.lat; do
 	isutf8 -q ${file} > /dev/null
 	OUT=$?
 	if [ $OUT -eq 1 ];then
 		$CONVERT   "$file"   -o  "${file}.utf8"
+		mv $file ../src/iso-encoded/
+		mv ${file}.utf8 ${file}
 	fi
 done
 exit 0
